d856a961d4555696875a3aa430699e8da860c8f4
shin0624 <91828379+shin0624@users.noreply.github.com>
2021-11-08 00:47:25 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,37 @@
 ﻿using System;
 
-namespace while문_연습
+namespace 소수판별코드_연습
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string answer;
-            do
+            
+            Console.WriteLine("입력된 숫자의 소수 유무를 판단하는 프로그램입니다.\n소수:1과 자기자신만으로 나누어떨어지는 수");
+
+           int num = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=0; i<=num;i++)
             {
-                Console.WriteLine("강사님은 잘생기셨나요?(y/n) : ");
-                 answer = Console.ReadLine();
+                if(num==2)
+                {
+                    Console.WriteLine("소수입니다!(2는 유일한 짝수소수)");
+                    break;
+                }
+                if((num%2)==0)
+                {
+                    Console.WriteLine("소수가 아닙니다!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("소수입니다!");
+                    break;
+                }
+
+            }
 
 
-            } while (answer != "y");
-            Console.WriteLine("정답입니다!");
         }
-
     }
 }
