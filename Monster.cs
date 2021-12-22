b4ccb64aecb9775cb5bf290178701ec8ce10d76b
shin0624 <91828379+shin0624@users.noreply.github.com>
2021-12-22 22:52:12 +0900

    Add files via upload

---
+++ Monster.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Text_RPG_2_플레이어_생성
+{
+    class Monster
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Text_RPG_2_플레이어_생성
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+        }
+    }
+}
