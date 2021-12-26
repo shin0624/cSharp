292813e93ebe3fe97bbaa320567d46b612fdb9fc
shin0624 <91828379+shin0624@users.noreply.github.com>
2021-12-26 22:28:38 +0900

    Add files via upload

---
+++ Creature.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Text_RPG_2_플레이어_생성
+{
+    public enum CreatureType
+    {
+        None,
+        Player =1,
+        Monster = 2
+    }
+    class Creature
+    { 
+        CreatureType type;
+
+        protected int hp = 0;
+        protected int attack = 0;
+
+        protected Creature(CreatureType type)
+        {
+            this.type = type;
+        }
+
+        public void SetInfo(int hp, int attack)
+        {
+            this.hp = hp;
+            this.attack = attack;
+        }
+
+        public int GetHp() { return hp; }
+        public int GetAttack() { return attack; }
+
+        public bool IsDead() { return hp <= 0; }
+
+        public void OnDamaged(int damage)
+        {
+            hp -= damage;
+            if (hp < 0)
+                hp = 0;
+        }
+
+    }
+}
---
+++ Monster.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Text_RPG_2_플레이어_생성
+{
+    public enum MonsterType
+    {
+        None = 0,
+        Slime = 1,
+        Ork = 2,
+        Skeleton = 3
+    }
+    
+    class Monster : Creature
+    {
+        protected MonsterType type;
+        protected Monster(MonsterType type) : base(CreatureType.Monster)
+        {
+            this.type = type;
+        }
+        MonsterType GetMonsterType() { return type; }
+    }
+
+    class Slime : Monster
+    {
+        public Slime() : base(MonsterType.Slime)
+        {
+            SetInfo(10, 1);
+        }
+    }
+    class Ork : Monster
+    {
+        public Ork() : base(MonsterType.Ork)
+        {
+            SetInfo(20, 2);
+        }
+    }
+    class Skeleton : Monster
+    {
+        public Skeleton() : base(MonsterType.Skeleton)
+        {
+            SetInfo(15, 5);
+        }
+    }
+
+}
